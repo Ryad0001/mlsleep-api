ea80b39469c39f852e9fb8e0db96c97f9e58cfee
Ryad0001 <160721966+Ryad0001@users.noreply.github.com>
2025-06-16 17:56:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ def train():
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=43)
         
-        mlflow.set_tracking_uri("file:/home/site/wwwroot/mlruns")
+        mlflow.set_tracking_uri("file:/tmp/mlruns")
 
         mlflow.set_experiment("sleep_disorder_classification")
 
